891d28d360b173f82512e9f6f7895212e86bcb53
anastasia <smolina.anastasia.14@gmail.com>
2021-05-23 15:52:43 +0300

    add comments

--- week29/src/components/Comments.jsx
+++ week29/src/components/Comments.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-
+import './Comments.css';
 export default class Comments extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             comments: [],
-            data: {
-                name: "",
-                comment: ""
-            }
+            username: "",
+            comment: ""
         }
     }
 
@@ -18,49 +16,63 @@ export default class Comments extends React.Component {
         }
     }
 
+    // связка name у инпута и value, универсальный способ вносить в инпут изменения
     handleChange = (e) => {
-        console.log(e.target.name)
         this.setState({
-            data: {
-            ...this.state.data,
-            [e.target.name]: e.target.value,
-        }
+            [e.target.name]: e.target.value
         })
     }
 
-    addComment = () => {
+    // addComment = () => {
+    //     this.setState({
+    //         // пуш в массив
+    //         comments: [
+    //             ...this.state.comments,
+    //             {
+    //                 username: this.state.username,
+    //                 comment: this.state.comment,
+    //             }
+    //         ],
+    //         // очищает поля ввода
+    //             username: '',
+    //             comment: ''
+    //         }, () => localStorage.setItem('comments', JSON.stringify(this.state)) // запись в хранилище
+    //     )
+    // }
+
+    // без спреда
+    addComment2 = () => {
+        let comments = this.state.comments;
+        comments.unshift({
+            username: this.state.username,
+            comment: this.state.comment,
+        })
         this.setState({
-            // клонируются объекты-комментарии в массив с помощью spread
-            comments: [
-                ...this.state.comments,
-                {
-                    name: this.state.data.name,
-                    comment: this.state.data.comment,
-                }
-            ],
-            // очищает поля ввода
-            data: {
-                name: '',
+            comments: comments,
+                username: '',
                 comment: ''
-            }
-        }, () => localStorage.setItem('comments', JSON.stringify(this.state)) // запись в хранилище
+            }, () => localStorage.setItem('comments', JSON.stringify(this.state)) // запись в хранилище
         )
     }
 
     render () {
         return (
             <div className="comment-box">
-                <p>your comment:</p>
-                {this.state.comments.map(comment =>
+                <p>Оставьте комментарий</p>
+                <form className="comment-form">
+                    <input value={this.state.username} type="text" name="username" className="data-name" placeholder="ваше имя" onChange={this.handleChange}></input>
+                    <textarea value={this.state.comment} type="text" name="comment" className="data-comment" placeholder="что вы об этом думаете?" onChange={this.handleChange}></textarea>
+                    <button onClick={this.addComment2} type="button">send</button>
+                </form>
+                <p>все комментарии:</p>
+                {this.state.comments.map((comment, i) =>
                 <div className="comments">
-                    <span>{comment.name}</span> : <span>{comment.comment}</span>
+                    {i === 0 ?
+                    <span className="new-comment">{comment.username} : {comment.comment}</span> :
+                    <span className="plain-comment">{comment.username} : {comment.comment}</span>} 
                 </div>
                 )}
-                <form>
-                    <input value={this.state.data.name} type="text" name="name" className="data-name" onChange={this.handleChange}></input>
-                    <textarea value={this.state.data.comment} type="text" name="comment" className="data-comment" onChange={this.handleChange}></textarea>
-                    <button onClick={this.addComment}>send</button>
-                </form>
+                
             </div>
         )
     }
